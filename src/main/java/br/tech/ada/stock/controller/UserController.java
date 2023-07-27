0e64caa7bb4807ad469c708694f35cf734df8cbf
arthurpdesimone <arthurpdesimone@gmail.com>
2023-07-26 23:29:15 -0300

    Version 0.2 : User creation endpoint

--- src/main/java/br/tech/ada/stock/controller/UserController.java
+++ src/main/java/br/tech/ada/stock/controller/UserController.java
@@ -1,7 +1,22 @@
 package br.tech.ada.stock.controller;
 
+import br.tech.ada.stock.model.User;
+import br.tech.ada.stock.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/create")
+    public String addUser(User user) {
+        userRepository.save(user);
+        return "Success";
+    }
+
 }
